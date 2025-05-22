376bace2b1c4b88a1eac65981a60a391ba22f1df
Mercy <mercymumbe17@gmail.com>
2025-05-22 23:04:23 +0300

    email passer regex

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+        email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+
+      
+        email_list = re.findall(email_pattern, self.email_string)
+
+        unique_emails = list(set(email_list))
+
+        unique_emails.sort()
+
+        return unique_emails
